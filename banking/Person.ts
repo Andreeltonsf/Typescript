e9ae6a26c59d5c31ff74b7591074338e230df9af
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2025-04-07 06:39:33 -0300

    Classes - Métodos e mutabilidade

--- banking/Person.ts
+++ banking/Person.ts
@@ -10,4 +10,24 @@ export class Person{
   }
   //Só posso ter um constructor - não pode haver mais de um
   //Constructor seria um método e não uma função
+
+
+  getFullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  getAge(): number {
+    const today = new Date();
+    const birthDate = this.birthDate;
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const isBirthdayPassed =
+      today.getMonth() > this.birthDate.getMonth() ||
+      (today.getMonth() === this.birthDate.getMonth() &&
+        today.getDate() >= this.birthDate.getDate());
+    return isBirthdayPassed ? age : age - 1;
+  }
+
+  updateFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
 }
