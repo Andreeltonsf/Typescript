7af4f27f41e6a1625a01061d6054b5c609ac66ed
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2025-04-07 06:17:52 -0300

    Paradigma Funcional e Depuração

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
-function main() {
-	function add(a: number, b: number): number {
-		return a + b;
-	}
+import {
+	type Person,
+	getAge,
+	getFullName,
+	updateFirstName,
+} from "./banking/person";
 
-	console.log(add(1, 2));
-	console.log("Hello World");
+function Main() {
+	const Maria: Person = {
+		firstName: "Maria",
+		lastName: "Rodriguez",
+		birthDate: new Date(1985, 1, 1),
+	};
+
+	console.log(getFullName(Maria));
+	console.log(getAge(Maria));
+
+	const updateMariA = updateFirstName(Maria, "Maria A");
+	console.log(getFullName(updateMariA));
+	console.log(getAge(updateMariA));
 }
-
-main();
