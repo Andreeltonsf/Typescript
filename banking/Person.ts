801721a61b5b9013147330f3a6cd5963d0848b9b
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2025-04-09 08:34:03 -0300

    Encapsulamento getters e setters

--- banking/Person.ts
+++ banking/Person.ts
@@ -1,33 +1,40 @@
-export class Person{
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
+export class Person {
+	private _firstName: string;
+	private lastName: string;
+	private birthDate: Date;
 
-  constructor(firstName: string, lastName: string, birthDate: Date) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthDate = birthDate;
-  }
-  //Só posso ter um constructor - não pode haver mais de um
-  //Constructor seria um método e não uma função
+	constructor(firstName: string, lastName: string, birthDate: Date) {
+		this._firstName = firstName;
+		this.lastName = lastName;
+		this.birthDate = birthDate;
+	}
+	//Só posso ter um constructor - não pode haver mais de um
+	//Constructor seria um método e não uma função
 
+	get firstname() {
+		return this._firstName;
+	}
 
-  getFullName(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
+	set nametag(tag: string) {
+		this.lastName = tag;
+	}
 
-  getAge(): number {
-    const today = new Date();
-    const birthDate = this.birthDate;
-    const age = today.getFullYear() - birthDate.getFullYear();
-    const isBirthdayPassed =
-      today.getMonth() > this.birthDate.getMonth() ||
-      (today.getMonth() === this.birthDate.getMonth() &&
-        today.getDate() >= this.birthDate.getDate());
-    return isBirthdayPassed ? age : age - 1;
-  }
+	getFullName(): string {
+		return `${this._firstName} ${this.lastName}`;
+	}
 
-  updateFirstName(firstName: string): void {
-    this.firstName = firstName;
-  }
+	getAge(): number {
+		const today = new Date();
+		const birthDate = this.birthDate;
+		const age = today.getFullYear() - birthDate.getFullYear();
+		const isBirthdayPassed =
+			today.getMonth() > this.birthDate.getMonth() ||
+			(today.getMonth() === this.birthDate.getMonth() &&
+				today.getDate() >= this.birthDate.getDate());
+		return isBirthdayPassed ? age : age - 1;
+	}
+
+	updateFirstName(firstName: string): void {
+		this._firstName = firstName;
+	}
 }
