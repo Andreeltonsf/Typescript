34e8e5a13bed7c3d3652a7a3ee08b8c0ffd443e1
Francisco ferreira <franciscoandreeltonsf@gmail.com>
2025-04-12 11:18:23 -0300

    Polimorfismo na prática

--- banking/BankAccount.ts
+++ banking/BankAccount.ts
@@ -3,12 +3,12 @@ import type { Person } from "./Person";
 // super class
 
 export class BankAccount {
-  static validateAccountNumber(accountNumber: string): any {
-    throw new Error("Method not implemented.");
-  }
+	static validateAccountNumber(accountNumber: string): any {
+		throw new Error("Method not implemented.");
+	}
 	private holder: Person; // composition (composição)
 
-	private _balance: number; // saldo - $100
+	protected _balance: number; // saldo - $100
 
 	private accountNumber: string; // número da conta
 
@@ -42,6 +42,14 @@ export class BankAccount {
 		} else {
 			console.log("Invalid withdrawal amount or insufficient funds.\n");
 		}
+	}
+
+	protected logSucessWithDraw(amount: number) {
+		console.log(`Withdrew $${amount}. New balance: $${this._balance}\n`);
+	}
+
+	protected logFailWithDraw(amount: number) {
+		console.log("Invalid withdrawal amount or insufficient funds.\n");
 	}
 
 	get balance(): number {
